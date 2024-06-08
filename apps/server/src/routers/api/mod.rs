6fb8abcf6e3f1ad2ce78d547a197f662c8026b1b
Joseph Micheli <josephwmicheli@gmail.com>
2024-06-08 14:23:06 -0500

    Basic simplification of codegen - need to move out of tests

--- apps/server/src/routers/api/mod.rs
+++ apps/server/src/routers/api/mod.rs
@@ -10,10 +10,15 @@ pub(crate) fn mount(app_state: AppState) -> Router<AppState> {
 
 #[allow(unused_imports)]
 mod tests {
-	use std::{fs::File, io::Write, path::PathBuf};
+	use std::{
+		fs::File,
+		io::{Read, Write},
+		path::PathBuf,
+	};
 
 	use specta::{
-		ts::{export, BigIntExportBehavior, ExportConfiguration, TsExportError},
+		export,
+		ts::{BigIntExportBehavior, ExportConfiguration, TsExportError},
 		NamedType,
 	};
 
@@ -23,150 +28,67 @@ mod tests {
 		UpdateCheck,
 	};
 
-	#[allow(dead_code)]
-	fn ts_export<T>() -> Result<String, TsExportError>
-	where
-		T: NamedType,
-	{
-		export::<T>(&ExportConfiguration::new().bigint(BigIntExportBehavior::BigInt))
-	}
-
 	#[test]
 	#[ignore]
 	fn codegen() -> Result<(), Box<dyn std::error::Error>> {
 		let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
 			.join("../../packages/types")
-			.join("generated.ts");
-
-		if !path.exists() {
-			panic!(
-				"Please run `cargo run --package codegen` first to generate the types"
-			);
-		}
+			.join("generated.ts")
+			.to_string_lossy()
+			.to_string();
 
 		println!(
 			"Please ensure to only generate types using `cargo run --package codegen`"
 		);
 
-		let mut file = std::fs::OpenOptions::new()
-			.write(true)
-			.append(true)
-			.open(path)?;
+		// Write all specta `Type` structs to generated_server_types.ts
+		export::ts_with_cfg(
+			&path,
+			&ExportConfiguration::default().bigint(BigIntExportBehavior::BigInt),
+		)
+		.unwrap();
 
-		file.write_all(b"// SERVER TYPE GENERATION\n\n")?;
-
-		file.write_all(format!("{}\n\n", ts_export::<ClaimResponse>()?).as_bytes())?;
-		file.write_all(format!("{}\n\n", ts_export::<StumpVersion>()?).as_bytes())?;
-		file.write_all(format!("{}\n\n", ts_export::<UpdateCheck>()?).as_bytes())?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<LoginOrRegisterArgs>()?).as_bytes(),
-		)?;
-		file.write_all(format!("{}\n\n", ts_export::<CreateUser>()?).as_bytes())?;
-		file.write_all(format!("{}\n\n", ts_export::<UpdateUser>()?).as_bytes())?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<UpdateUserPreferences>()?).as_bytes(),
-		)?;
-		file.write_all(format!("{}\n\n", ts_export::<DeleteUser>()?).as_bytes())?;
-
-		file.write_all(
-			format!("{}\n\n", ts_export::<EmailerIncludeParams>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<EmailerSendRecordIncludeParams>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<SendAttachmentEmailsPayload>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<SendAttachmentEmailResponse>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateOrUpdateEmailer>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateOrUpdateEmailDevice>()?).as_bytes(),
-		)?;
-		file.write_all(format!("{}\n\n", ts_export::<PatchEmailDevice>()?).as_bytes())?;
-
-		file.write_all(format!("{}\n\n", ts_export::<CreateLibrary>()?).as_bytes())?;
-		file.write_all(format!("{}\n\n", ts_export::<UpdateLibrary>()?).as_bytes())?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<UpdateLibraryExcludedUsers>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CleanLibraryResponse>()?).as_bytes(),
-		)?;
-		file.write_all(format!("{}\n\n", ts_export::<LibraryStatsParams>()?).as_bytes())?;
-
-		file.write_all(
-			format!("{}\n\n", ts_export::<PutMediaCompletionStatus>()?).as_bytes(),
-		)?;
-		file.write_all(format!("{}\n\n", ts_export::<MediaIsComplete>()?).as_bytes())?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<MediaMetadataOverview>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateOrUpdateBookmark>()?).as_bytes(),
-		)?;
-		file.write_all(format!("{}\n\n", ts_export::<DeleteBookmark>()?).as_bytes())?;
-		file.write_all(format!("{}\n\n", ts_export::<SeriesIsComplete>()?).as_bytes())?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<UpdateSchedulerConfig>()?).as_bytes(),
-		)?;
-
-		file.write_all(format!("{}\n\n", ts_export::<GetBookClubsParams>()?).as_bytes())?;
-		file.write_all(format!("{}\n\n", ts_export::<CreateBookClub>()?).as_bytes())?;
-		file.write_all(format!("{}\n\n", ts_export::<UpdateBookClub>()?).as_bytes())?;
-		// file.write_all(
-		// 	format!("{}\n\n", ts_export::<UpdateBookClubSchedule>()?).as_bytes(),
-		// )?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateBookClubInvitation>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<BookClubInvitationAnswer>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateBookClubMember>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<UpdateBookClubMember>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateBookClubScheduleBookOption>()?)
-				.as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateBookClubScheduleBook>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateBookClubSchedule>()?).as_bytes(),
-		)?;
-
-		file.write_all(
-			format!("{}\n\n", ts_export::<PatchMediaThumbnail>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<PatchSeriesThumbnail>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<PatchLibraryThumbnail>()?).as_bytes(),
-		)?;
-
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateOrUpdateSmartList>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<GetSmartListsParams>()?).as_bytes(),
-		)?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<SmartListRelationOptions>()?).as_bytes(),
-		)?;
-		file.write_all(format!("{}\n\n", ts_export::<SmartListMeta>()?).as_bytes())?;
-		file.write_all(
-			format!("{}\n\n", ts_export::<CreateOrUpdateSmartListView>()?).as_bytes(),
-		)?;
+		// Do postprocessing to apply changes to specific lines
+		codegen_postprocessing(&path)?;
 
 		Ok(())
+	}
+
+	fn codegen_postprocessing(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+		// Read the file content
+		let mut file = File::open(path)?;
+		let mut file_content = String::new();
+		file.read_to_string(&mut file_content)?;
+
+		// Run each replacement function
+		file_content = replace_core_job_output_line(file_content);
+		file_content = replace_book_club_member_role_spec(file_content);
+		file_content = wrap_codegen_with_header(file_content);
+
+		// Reopen the file in write mode to overwrite the previous content
+		let mut file = File::create(path)?;
+		file.write_all(file_content.as_bytes())?;
+
+		Ok(())
+	}
+
+	fn replace_core_job_output_line(file_content: String) -> String {
+		// Replace the line to remove `any`
+		file_content.replace(
+        "export type CoreJobOutput = LibraryScanOutput | SeriesScanOutput | ThumbnailGenerationOutput | any",
+        "export type CoreJobOutput = LibraryScanOutput | SeriesScanOutput | ThumbnailGenerationOutput"
+    )
+	}
+
+	fn replace_book_club_member_role_spec(file_content: String) -> String {
+		file_content.replace(
+			"export type BookClubMemberRoleSpec = { [key: BookClubMemberRole]: string }",
+			"export type BookClubMemberRoleSpec = Record<BookClubMemberRole, string>",
+		)
+	}
+
+	fn wrap_codegen_with_header(file_content: String) -> String {
+		let header = "/* eslint-disable @typescript-eslint/ban-types */";
+		format!("{}\n{}", header, file_content)
 	}
 }
